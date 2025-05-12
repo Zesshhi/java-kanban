fc5ceb9fbbb398ee088dcf23e48ce69ddc1db04b
Kirill <96735975+Zesshhi@users.noreply.github.com>
2025-05-13 00:50:37 +0300

    sprint_9 Поддержка HTTP запрос на весь функционал

--- src/http/handlers/SubTaskHandler.java
+++ src/http/handlers/SubTaskHandler.java
@@ -1,7 +1,6 @@
 package http.handlers;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.sun.net.httpserver.HttpExchange;
